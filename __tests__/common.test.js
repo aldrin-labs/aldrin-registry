6c8cc887442137df0808cff9e3b02c8a7d6c5995
Likbes <likbes.js@gmail.com>
2022-11-15 14:14:24 +0200

    testing common test file

--- __tests__/common.test.js
+++ __tests__/common.test.js
@@ -1,7 +1,13 @@
+import { markets } from 'aldrin-registry/src/markets.json'
+import { tokens } from 'aldrin-registry/src/tokens.json'
+
+console.log('markets', markets)
+console.log('tokens', tokens)
+
 test('1 + 1 = 2', () => {
   expect(1 + 1).toBe(2);
 });
 
 test('1 + 2 = 3', () => {
-  expect(1 + 2).toBe(3);
+  expect(1 + 3).toBe(3);
 });
